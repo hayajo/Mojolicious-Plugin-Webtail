65e0dea778c1c8486678660ddd4975b4a4e528c9
hayajo <hayajo@cpan.org>
2013-02-12 14:43:37 +0900

    Checking in changes prior to tagging of version 0.03.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index c419d9a..be3fdf5 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Mojolicious::Plugin::Webtail
    
    +0.03
    +        - Fixed a bug #83245
    +
     0.02
             - Added bin/mwtail

--- lib/Mojolicious/Plugin/Webtail.pm
+++ lib/Mojolicious/Plugin/Webtail.pm
@@ -2,7 +2,7 @@ package Mojolicious::Plugin::Webtail;
 
 use strict;
 use warnings;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use Mojo::Base 'Mojolicious::Plugin';
 use Mojo::Util qw{ slurp };
